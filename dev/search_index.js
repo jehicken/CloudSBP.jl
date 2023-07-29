c01afc7a932fc672535e5a274710c6e6b72defbb
Documenter.jl <documenter@juliadocs.github.io>
2023-07-29 15:30:24 +0000

    build based on 722d256

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CutDGD","category":"page"},{"location":"#CutDGD","page":"Home","title":"CutDGD","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CutDGD.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CutDGD]","category":"page"}]
+}
